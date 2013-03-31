cac5a9ee4a96a20ca0c9910a475e3e5f2e199d54
Rupesh S <rupesh.rupeshs@gmail.com>
2013-03-31 23:05:55 +0530

    removed shadow build

--- src/ui_midialog.h
+++ src/ui_midialog.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'midialog.ui'
 **
-** Created: Sun Feb 17 10:50:24 2013
+** Created: Sun Mar 31 23:00:55 2013
 **      by: Qt User Interface Compiler version 4.7.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
