7197c656492aff19eac517fd5651cc9032f3ac88
Rupesh S <rupesh.rupeshs@gmail.com>
2013-12-28 21:51:40 +0530

    Added update check

---
+++ src/updatecheck.cpp
@@ -0,0 +1,139 @@
+/*  exmplayer, GUI front-end for mplayer.
+    Copyright (C) 2010-2013 Rupesh Sreeraman
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "updatecheck.h"
+
+UpdateCheck::UpdateCheck(QObject *parent) :
+    QObject(parent)
+{
+
+    manager= new QNetworkAccessManager(this);
+    connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(fileIsReady(QNetworkReply*)) );
+
+}
+void UpdateCheck::fileIsReady( QNetworkReply * reply)
+{
+
+    reply->deleteLater();
+
+    if(reply->error() == QNetworkReply::NoError) {
+        // Get the http status code
+        int v = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+        // Here we got the final reply
+        QByteArray replyXml = reply->readAll();
+        qDebug()<<replyXml;
+
+        QXmlStreamReader reader(replyXml);
+        QString os;
+        QString version;
+        while (!reader.atEnd()) {
+
+            reader.readNext();
+            if (reader.isStartElement()) {
+                if (reader.name() == "OperatingSystem")
+                {
+                    os=reader.readElementText();
+                    //qDebug()<<p;
+                }
+                if (reader.name() == "StableVersion")
+                {
+                    version=reader.readElementText();
+
+                }
+                if (reader.name() == "ReleaseNotes")
+                {
+                    QString relNotes=reader.readElementText();
+                    QStringList vList;
+                    vList.append(version);
+                    vList.append(relNotes);
+                    mapVersions.insert(os,vList);
+                }
+            }
+
+        }
+    }
+    else
+    {
+        // Error
+        qDebug()<<reply->errorString();
+        if (reply->errorString()=="Host  not found")
+            emit NetworkError("Host  not found");
+
+
+    }
+
+    reply->manager()->deleteLater();
+
+
+#ifdef Q_OS_WIN
+    QString latestVersion;
+    QString myVersion=Version::stable();
+    if (mapVersions["Windows"].count()>0)
+        latestVersion= mapVersions["Windows"].at(0);
+    bool bUpdateAvail=isUpdated(latestVersion,myVersion);
+    if(mapVersions["Windows"].count()>1)
+        emit gotUpdateChkResponse(bUpdateAvail,mapVersions["Windows"].at(0),mapVersions["Windows"].at(1));
+
+#endif
+#ifdef Q_OS_LINUX
+    QString latestVersion;
+    QString myVersion=Version::stable();
+    if (mapVersions["Linux"].count()>0)
+        latestVersion= mapVersions["Linux"].at(0);
+    bool bUpdateAvail=isUpdated(latestVersion,myVersion);
+
+    if(mapVersions["Linux"].size()>1)
+        emit gotUpdateChkResponse(bUpdateAvail,mapVersions["Linux"].at(0),mapVersions["Linux"].at(1));
+
+# endif
+
+    if( bUpdateAvail)
+            _stableVersion=latestVersion;
+
+}
+void UpdateCheck::Check()
+{
+    QNetworkRequest request(QUrl("https://raw.github.com/rupeshs/exmplayer-update/master/update.xml"));
+    manager->get(request);
+}
+bool UpdateCheck::isUpdated(QString currentVersion,QString myVersion)
+{
+
+    QStringList cVl=  currentVersion.split(".");
+    QStringList myVl=  myVersion.split(".");
+
+    if (cVl.count()==3 && myVl.count()==3)
+    {
+        if(cVl.at(0).toInt()>myVl.at(0).toInt())
+        { //Major update found
+            return true;
+        }
+        else if (cVl.at(1).toInt()>myVl.at(1).toInt())
+        { //Minor update found
+            return true;
+        }
+        else if (cVl.at(2).toInt()>myVl.at(2).toInt())
+        {//Revision update found
+            return true;
+        }
+
+
+    }
+
+    return false;
+}
---
+++ src/updatecheckdlg.cpp
@@ -0,0 +1,74 @@
+/*  exmplayer, GUI front-end for mplayer.
+    Copyright (C) 2010-2013 Rupesh Sreeraman
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "updatecheckdlg.h"
+#include "ui_updatecheckdlg.h"
+
+updateCheckDlg::updateCheckDlg(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::updateCheckDlg)
+{
+    ui->setupUi(this);
+    updChecker=new UpdateCheck();
+    updChecker->Check();
+    QObject::connect(updChecker,SIGNAL(gotUpdateChkResponse(bool,QString,QString)),this,SLOT(gotUpdateChkResponse(bool,QString,QString)));
+    QObject::connect(updChecker,SIGNAL(NetworkError(QString)),this,SLOT(NetworkError(QString)));
+
+    pgIndicator=new QProgressIndicator(ui->labelPg);
+    pgIndicator->setColor(QColor(qRgb(10,100,10) ));
+    pgIndicator->show();
+    pgIndicator->startAnimation();
+    ui->labelCurVersion->setText("Current version :v" +Version::stable());
+}
+
+updateCheckDlg::~updateCheckDlg()
+{
+    delete ui;
+}
+void updateCheckDlg::gotUpdateChkResponse(bool updAvail,QString version,QString releaseNotes)
+{
+
+    pgIndicator->stopAnimation();
+    pgIndicator->hide();
+    if (updAvail)
+       {this->show();
+        ui->labelVersionFound->setText("<b>Update available,Version "+version+"<\b>");
+        QPropertyAnimation *animation = new QPropertyAnimation(this, "size");
+         animation->setDuration(500);
+         animation->setEndValue(QSize(371,263));
+         animation->start();
+       }
+    else
+       ui->labelVersionFound->setText("<font color=\"green\"><b>ExMplayer is up to date</b></font>");
+
+
+    QStringList sList=releaseNotes.split(",");
+    QString var;
+
+    foreach (var, sList) {
+        new QListWidgetItem(var, ui->listWidgetRel);
+    }
+
+}
+void updateCheckDlg::NetworkError(QString)
+{
+    pgIndicator->stopAnimation();
+    pgIndicator->hide();
+    ui->labelVersionFound->setText("<font color=\"red\"><b>Error...Please check your internet connection.</b></font>");
+
+}
